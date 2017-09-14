18ae702d33635d6b38ae3f3c3046d22f7e8caf3a
LuHong <cmanlh@163.com>
2017-09-14 17:55:47 +0800

    start working on server

--- ebmms-server/src/test/java/com/lifeonwalden/ebmms/server/ServerTest.java
+++ ebmms-server/src/test/java/com/lifeonwalden/ebmms/server/ServerTest.java
@@ -1,65 +1,23 @@
 package com.lifeonwalden.ebmms.server;
 
-import com.lifeonwalden.ebmms.common.codec.RequestDecoder;
-import com.lifeonwalden.ebmms.common.codec.ResponseEncoder;
-import com.lifeonwalden.ebmms.server.handler.MsgProcessor;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+@Configuration
+@ComponentScan("com.lifeonwalden")
 public class ServerTest {
-
-    private int port;
-
-    public ServerTest(int port) {
-        this.port = port;
-    }
-
     public static void main(String[] args) throws Exception {
-        int port;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        } else {
-            port = 8080;
-        }
-        new ServerTest(port).run();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(ServerTest.class);
+        System.out.println("Start Over");
+//        ((Server) appContext.getBean("server")).destroy();
     }
 
-    public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup(50);
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 8, 0, 8), new RequestDecoder())
-                                    .addLast(new LengthFieldPrepender(8), new ResponseEncoder())
-                                    .addLast(new MsgProcessor());
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-            // Bind and start to accept incoming connections.
-            ChannelFuture f = b.bind(port).sync();
-
-            // Wait until the server socket is closed.
-            // In this example, this does not happen, but you can do that to gracefully
-            // shut down your server.
-            f.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
